6e82e205fd2f8b762db5db3692324a27393e6364
Clemens Horn <clemens.horn@mni.thm.de>
2022-12-26 22:06:33 +0100

    axum hello world that consumes the grpc api
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use rust_api_models::*;
+
+#[tokio::main]
+async fn main() {
+    let req: Request = Request {
+        field: "Hello".to_owned(),
+        value: "World!".to_owned(),
+    };
+    let res = format!("{:#?}\n", req);
+    let app = Router::new().route("/", get(|| async { res }));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
