34437cba29be577fda37d589c1966a3b5b656a40
Clemens Horn <clemens.horn@mni.thm.de>
2022-12-26 14:08:08 +0100

    no fail if no rust affected
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 nx_exec() {
+    NEXT_TELEMETRY_DISABLED=1 \
     npx nx affected --parallel --target "$1"
 }
 
-nx format
-nx workspace-lint
+if npx nx print-affected --select=projects | sed "s/, /\n/g" | grep -E "^api$|^rust-[^,]*|^desktop$"; then
+    echo "RUST=true"
+fi
+
+npx nx format
+npx nx workspace-lint
 nx_exec build
 nx_exec lint
 nx_exec format
