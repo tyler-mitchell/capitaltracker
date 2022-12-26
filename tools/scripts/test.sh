31893b004c014b63e4aa94a61a22b45de72dcd24
Clemens Horn <clemens.horn@mni.thm.de>
2022-12-26 14:48:57 +0100

    fuck you
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -2,6 +2,7 @@
 
 nx_exec() {
     NEXT_TELEMETRY_DISABLED=1 \
+    NX_CLOUD_DISTRIBUTED_EXECUTION=false \
     npx nx affected --parallel --target "$1"
 }
 
@@ -9,8 +10,8 @@ if npx nx print-affected --select=projects | sed "s/, /\n/g" | grep -E "^api$|^r
     echo "RUST=true"
 fi
 
-npx nx format  --libs-and-apps
-npx nx workspace-lint
+NX_CLOUD_DISTRIBUTED_EXECUTION=false npx nx format  --libs-and-apps
+NX_CLOUD_DISTRIBUTED_EXECUTION=false npx nx workspace-lint
 nx_exec build
 nx_exec lint
 nx_exec format
