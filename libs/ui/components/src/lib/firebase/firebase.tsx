57e5b64892f267ba0692248dbf5fe92c91064165
Clemens Horn <clemens.horn@mni.thm.de>
2022-12-28 17:44:17 +0100

    abort workflow if runner fails
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/ui/components/src/lib/firebase/firebase.tsx
+++ libs/ui/components/src/lib/firebase/firebase.tsx
@@ -1,11 +1,8 @@
 import { initializeApp } from 'firebase/app';
+
 /* eslint-disable-next-line */
 export interface FirebaseProps {}
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: 'AIzaSyDRNBHw6LQnByFkUGEFkrOO0pOFRqrsvd4',
@@ -16,10 +13,10 @@ const firebaseConfig = {
     appId: '1:697425312434:web:a13883dd494fe4692c27ab',
     measurementId: 'G-FYB48VC2MJ',
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
 export function Firebase(props: FirebaseProps) {
-    console.log(app);
+    // Initialize Firebase
+    initializeApp(firebaseConfig);
     return null;
 }
 
