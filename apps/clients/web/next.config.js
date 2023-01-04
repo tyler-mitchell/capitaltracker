e1d3837e1b845ea459e8f1fa76e0bf42e8b93e49
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-05 00:43:59 +0100

    add next expo adapter support
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/clients/web/next.config.js
+++ apps/clients/web/next.config.js
@@ -1,15 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withExpo } = require('@expo/next-adapter');
 const { withNx } = require('@nrwl/next/plugins/with-nx');
+const withTM = require('next-transpile-modules')([]);
+const withPlugins = require('next-compose-plugins');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+    // reanimated (and thus, Moti) doesn't work with strict mode currently...
+    // https://github.com/nandorojo/moti/issues/224
+    // https://github.com/necolas/react-native-web/pull/2330
+    // https://github.com/nandorojo/moti/issues/224
+    // once that gets fixed, set this back to true
+    reactStrictMode: false,
     nx: {
         // Set this to true if you would like to to use SVGR
         // See: https://github.com/gregberge/svgr
-        svgr: false,
+        svgr: true,
     },
+    // swcMinify: true,
+    // experimental: {
+    //     forceSwcTransforms: true, // set this to true to use reanimated + swc experimentally
+    //     swcPlugins: [[require.resolve('./plugins/swc_plugin_reanimated.wasm')]],
+    // },
 };
 
-module.exports = withNx(nextConfig);
+module.exports = async (phase, { defaultConfig }) =>
+    withPlugins([withTM, withExpo], withNx(nextConfig))(phase, {
+        ...defaultConfig,
+        ...nextConfig,
+    });
