9ddc62517aa7a0ec043ea37c73aacb2ab6a6f7c8
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-05 00:46:16 +0100

    next config can be extended with plugins, supports expo
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/clients/web/next.config.js
+++ apps/clients/web/next.config.js
@@ -1,7 +1,7 @@
 const { withExpo } = require('@expo/next-adapter');
 const { withNx } = require('@nrwl/next/plugins/with-nx');
-const withTM = require('next-transpile-modules')([]);
 const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([]);
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -12,17 +12,17 @@ const nextConfig = {
     // https://github.com/necolas/react-native-web/pull/2330
     // https://github.com/nandorojo/moti/issues/224
     // once that gets fixed, set this back to true
-    reactStrictMode: false,
-    nx: {
-        // Set this to true if you would like to to use SVGR
-        // See: https://github.com/gregberge/svgr
-        svgr: true,
-    },
+    // reactStrictMode: true,
     // swcMinify: true,
     // experimental: {
     //     forceSwcTransforms: true, // set this to true to use reanimated + swc experimentally
     //     swcPlugins: [[require.resolve('./plugins/swc_plugin_reanimated.wasm')]],
     // },
+    nx: {
+        // Set this to true if you would like to to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: true,
+    },
 };
 
 module.exports = async (phase, { defaultConfig }) =>
