14d75fccee19d40faabac32cc74d699df690bcf3
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-06 15:25:36 +0100

    updated web, added document, favicon, removed warnings from css tailwind imports, added styles lib with tailwind theme
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from './components/footer/footer';
 export * from './components/header/header';
 export * from './pages/user/user';
 export * from './pages/home/home';
+export * from './styles/theme';
---
+++ libs/shared/src/styles/theme.ts
@@ -0,0 +1,6 @@
+const theme: import('tailwindcss').Config['theme'] = {
+    // edit your tailwind theme here!
+    // https://tailwindcss.com/docs/adding-custom-styles
+};
+
+export default theme;
