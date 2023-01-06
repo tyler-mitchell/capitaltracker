a6cefc2bdee4a7815d8385e3b6dec9d9e4fbf94a
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-06 14:38:10 +0100

    updated next and removed unnecessary tailwind glob pattern
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/clients/web/jest.config.ts
+++ apps/clients/web/jest.config.ts
@@ -1,13 +1,13 @@
 module.exports = {
     displayName: 'web',
     resolver: '@nrwl/jest/plugins/resolver',
+    preset: 'jest-expo/web',
     moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
     coverageDirectory: '../../../coverage/apps/clients/web',
-    moduleNameMapper: {
-        '\\.svg$': '@nrwl/expo/plugins/jest/svg-mock',
-    },
-    preset: 'jest-expo/web',
     transformIgnorePatterns: [
         'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
     ],
+    moduleNameMapper: {
+        '\\.svg$': '@nrwl/expo/plugins/jest/svg-mock',
+    },
 };
