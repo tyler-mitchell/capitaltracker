751be6ae6bda9bd167feb6a70a13a92dfc8ae501
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-07 22:07:36 +0100

    update timeout for ci to 5 minutes
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/e2e/web/src/e2e/app.cy.ts
+++ apps/e2e/web/src/e2e/app.cy.ts
@@ -1,5 +1,5 @@
 describe('capitaltracker', () => {
-    beforeEach(() => cy.visit('/'));
+    beforeEach(() => cy.visit('/', {timeout: 300000}));
     it('should display welcome message', () => {
         cy.get('h1[data-testid="heading"]').should('contain', 'Welcome to Solito.');
     });
