688af843aecd119ed6ad6fc4711163815edb6353
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-07 22:09:51 +0100

    format
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/e2e/web/src/e2e/app.cy.ts
+++ apps/e2e/web/src/e2e/app.cy.ts
@@ -1,5 +1,5 @@
 describe('capitaltracker', () => {
-    beforeEach(() => cy.visit('/', {timeout: 300000}));
+    beforeEach(() => cy.visit('/', { timeout: 300000 }));
     it('should display welcome message', () => {
         cy.get('h1[data-testid="heading"]').should('contain', 'Welcome to Solito.');
     });
