4521db7af54c3c81761dd1e3710145e875bddf99
Clemens <51924045+clemenscodes@users.noreply.github.com>
2023-01-08 17:47:43 +0100

    add commit hooks (#41)
    - add husky
    - edit precommit
    - edit precommit
    - edit precommit
    - edit precommit
    - add husky
    - feat(repo): add commitizen
    - chore(root): add commitlint adapter to commitizen
    - test
    - 🧹 chore(repo): test
    - 🧹 chore(failing scope): test
    - 🧹 chore(repo): pas
    - 🧹 chore(should fail): fail
    - 🧹 chore(fail): fail
    - 🧹 chore(fail): fail
    - 🧹 chore(fail): fail
    - 🧹 chore(repo): configure commitlint hooks
    
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tools/scripts/ci.sh
+++ tools/scripts/ci.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-if nx print-affected --select=projects | sed "s/, /\n/g" | grep -E "^api$|^rust-[^,]*|^desktop$"; then
-    echo "RUST=true"
-fi
-
-nx format --libs-and-apps
-nx workspace-lint
-yarn affected build --parallel "$1"
-yarn affected lint --parallel "$1"
-yarn affected test --parallel "$1"
+nx format --libs-and-apps &&
+nx workspace-lint &&
+yarn affected build --nx-bail --parallel "$1" &&
+yarn affected lint --nx-bail --parallel "$1" &&
+yarn affected test --nx-bail  --parallel "$1" &&
 yarn affected e2e --parallel 1
